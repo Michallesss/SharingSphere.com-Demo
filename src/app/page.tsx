2964aa1dd32ebf99615d3118ea4f2f5bf5e23275
Antoni Kijjaroentorn <antoni.kijjaroentorn@dxc.com>
2024-05-19 21:37:03 +0200

    ✅ add sub newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Preview from '@/components/Preview';
 import Faq from "@/components/Faq";
-import Waitlist from "@/components/Waitlist";
+import Waitlist from "@/components/LastCta";
 import Footer from "@/components/Footer";
 import { NavbarMenu } from "@/components/NavbarMenu";
 
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -8,18 +8,15 @@ const Preview = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-3xl font-bold text-blue-600 mb-4">
-            Long headline on two lines to turn your visitors into users and
-            achieve more
+            Expand Your Reach and Maximize Your Social Media Impact.
           </h2>
           <p className="text-lg text-gray-700 mb-8">
-            Separated they live in Bookmarks right at the coast of the famous
-            Semantics, large language ocean
+            Our app provides a seamless way to share your content across
+            multiple platforms effortlessly.
           </p>
           <ul className="list-disc list-inside text-gray-700 space-y-2">
-            <li>Showcase and embed your work with</li>
-            <li>Publish across social channels in a click</li>
-            <li>Sell your videos worldwide</li>
-            <li>Embed your work with</li>
+            <li>Publish across social channels with a click</li>
+            <li>Embed your work with ease</li>
           </ul>
         </div>
         <div className="md:w-1/2 mt-8 md:mt-0 flex justify-center">
